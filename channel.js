ece44495ead8c77f47431dec72dc5a82b6b9daf5
Daniele Polencic <daniele.polencic@gmail.com>
2015-06-17 23:20:35 +0100

    first commit

---
+++ channel.js
@@ -0,0 +1,77 @@
+'use strict';
+
+module.exports = Channel;
+
+function Channel() {
+  return {
+    buffer: [],
+    consumers: [],
+    closed: false
+  };
+}
+
+const END = Channel.END = Symbol('END');
+const KEEP_OPEN = Channel.KEEP_OPEN = Symbol('KEEP_OPEN');
+const CLOSE_BOTH = Channel.CLOSE_BOTH = Symbol('CLOSE_BOTH');
+
+Channel.put = (channel, value) => {
+  if (channel.closed) return channel;
+
+  if (value === END) channel.closed = true;
+
+  channel.buffer.push(value);
+  return runTick(channel);
+};
+
+Channel.take = (channel, ...fns) => {
+  if (channel.closed && channel.buffer.length === 0) return channel;
+
+  const takeOneMore = (value) => {
+    if (value !== END) Channel.take.apply(void 0, [channel].concat(fns));
+    return value;
+  };
+
+  const consumer = pipeline([takeOneMore].concat(fns));
+  channel.consumers.push(consumer);
+
+  return runTick(channel);
+};
+
+Channel.close = (channel) => {
+  return Channel.put(channel, END);
+};
+
+Channel.pipe = (input, output, keepOpen = KEEP_OPEN, transform = identity) => {
+  Channel.take(input, (value) => {
+    if (!(keepOpen === KEEP_OPEN && value === END))
+      Channel.put(output, transform(value));
+  });
+  return output;
+};
+
+Channel.merge = (channels, output, keepOpen, transform) => {
+  channels.forEach((channel) => Channel.pipe(channel, output, keepOpen, transform));
+  return output;
+};
+
+const runTick = (channel) => {
+  if (!(channel.buffer.length !== 0 && channel.consumers.length !== 0)) {
+    return channel;
+  }
+
+  const message = channel.buffer.shift();
+
+  for (let i = 0, len = channel.consumers.length; i < len; i += 1) {
+    setImmediate(channel.consumers.shift().bind(null, message));
+  }
+
+  return channel;
+};
+
+const identity = (value) => value;
+
+const pipeline = (fns) => {
+  return (seed) => {
+    return fns.reduce((previous, current) => current.call(void 0, previous), seed);
+  }
+};
---
+++ test.js
@@ -0,0 +1,186 @@
+var test = require('tape');
+var Channel = require('./channel.js');
+
+test('Channel() creates a new channel', (assert) => {
+  assert.plan(3);
+
+  const channel = Channel();
+  assert.deepEqual(channel.buffer, []);
+  assert.deepEqual(channel.consumers, []);
+  assert.ok(!channel.closed);
+});
+
+test('Channel.put puts a value into a channel', (assert) => {
+  assert.plan(1);
+
+  let channel = Channel();
+  Channel.put(channel, 42);
+  assert.deepEqual(channel.buffer, [42]);
+});
+
+test('Channel.put puts END into a channel', (assert) => {
+  assert.plan(1);
+
+  let channel = Channel();
+  Channel.put(channel, 52);
+  Channel.put(channel, Channel.END);
+  Channel.put(channel, 53);
+  assert.deepEqual(channel.buffer, [52, Channel.END]);
+});
+
+test('Channel.take takes a value from a channel', (assert) => {
+  assert.plan(1);
+
+  let channel = Channel();
+  Channel.put(channel, 43);
+  Channel.take(channel, (value) => assert.equal(value, 43));
+});
+
+test('Channel.take takes values from a channel', (assert) => {
+  assert.plan(2);
+
+  let channel = Channel();
+  let i = 0;
+  Channel.put(channel, 91);
+  Channel.put(channel, 92);
+  Channel.take(channel, (value) => {
+    if (i === 0) {
+      assert.equal(value, 91);
+      i++;
+    } else {
+      assert.equal(value, 92);
+    }
+  });
+});
+
+test('Channel.take can fire multiple callbacks', (assert) => {
+  assert.plan(1);
+
+  let channel = Channel();
+  Channel.put(channel, 100);
+  Channel.take(channel, (value) => (value / 5), (value) => assert.equal(value, 20));
+});
+
+test('Channel.take ignores values from closed channel with consumers', (assert) => {
+  assert.plan(2);
+
+  let channel = Channel();
+  Channel.take(channel, (value) => {
+    Channel.take(channel, () => assert.fail('shoul not have been called'));
+    assert.strictEqual(value, Channel.END);
+  });
+  Channel.close(channel);
+  Channel.put(channel, 57);
+  assert.deepEqual(channel.buffer, []);
+});
+
+test('Channel.take ignores values from closed channel with values', (assert) => {
+  assert.plan(4);
+
+  let channel = Channel();
+  let i = 0;
+  Channel.put(channel, 75);
+  Channel.close(channel);
+  assert.deepEqual(channel.buffer, [75, Channel.END]);
+  Channel.take(channel, (value) => {
+    if (i === 0) {
+      assert.equal(value, 75)
+      i++;
+    } else {
+      assert.strictEqual(value, Channel.END);
+      assert.deepEqual(channel.buffer, []);
+    }
+  });
+});
+
+test('Channel.close closes a channel', (assert) => {
+  assert.plan(2);
+
+  let channel = Channel();
+  Channel.close(channel);
+  Channel.put(channel, 58);
+  Channel.take(channel, (value) => assert.strictEqual(value, Channel.END));
+  assert.deepEqual(channel.buffer, []);
+});
+
+test('Channel.pipe pipes values from input to output', (assert) => {
+  assert.plan(1);
+
+  let input = Channel();
+  let output = Channel();
+  Channel.pipe(input, output);
+  Channel.put(input, 44);
+  Channel.take(output, (value) => assert.equal(value, 44));
+});
+
+test('Channel.pipe transforms piped values', (assert) => {
+  assert.plan(1);
+
+  let input = Channel();
+  let output = Channel();
+  Channel.pipe(input, output, Channel.KEEP_OPEN, ((x) => x / 5));
+  Channel.put(input, 45);
+  Channel.take(output, (value) => assert.equal(value, 9));
+});
+
+test('Channel.pipe pipes END to input and output', (assert) => {
+  assert.plan(3);
+
+  let input = Channel();
+  let output = Channel();
+  Channel.pipe(input, output, Channel.CLOSE_BOTH);
+  Channel.put(input, Channel.END);
+  Channel.take(output, (value) => {
+    Channel.put(input, 59);
+    Channel.put(output, 59);
+    assert.strictEqual(value, Channel.END);
+    assert.deepEqual(input.buffer, []);
+    assert.deepEqual(output.buffer, []);
+  });
+});
+
+test('Channel.pipe does not pipe values to a closed output channel', (assert) => {
+  assert.plan(2);
+
+  let input = Channel();
+  let output = Channel();
+  Channel.pipe(input, output);
+  Channel.close(output);
+  Channel.put(input, 201);
+  Channel.put(output, 201);
+  Channel.take(output, (value) => assert.strictEqual(value, Channel.END));
+  assert.deepEqual(output.buffer, []);
+});
+
+test('Channel.pipe does not pipe END to output', (assert) => {
+  assert.plan(1);
+
+  let input = Channel();
+  let output = Channel();
+  Channel.pipe(input, output);
+  Channel.put(input, Channel.END);
+  Channel.put(output, 202);
+  Channel.take(output, (value) => assert.equal(value, 202));
+  Channel.take(input, () => assert.fail('should not have been called'));
+});
+
+test('Channel.merge merges multiple channels into one', (assert) => {
+  assert.plan(2);
+
+  let one = Channel();
+  let two = Channel();
+  let i = 0;
+  let output = Channel();
+  Channel.merge([one, two], output);
+  Channel.take(output, (value) => {
+    if (i === 0) {
+      assert.equal(value, 46);
+      i += 1;
+    } else {
+      assert.equal(value, 47);
+    }
+  });
+
+  Channel.put(one, 46);
+  Channel.put(two, 47);
+});
